a260b7a60678651fa63fa6d6c38dc18d50581d5c
shwhwdhdsd <113272624+shwhwdhdsd@users.noreply.github.com>
2022-09-12 14:18:12 +0800

    Update 11.cpp

--- Sqlist/11.cpp
+++ Sqlist/11.cpp
@@ -6,23 +6,18 @@ int fun(int A[], int n)
   int *B = new int[n];
   for (int i = 0; i < n; ++i)
     B[i] = 0;
-  int i, k, max = 0;
+  int i, k=0, max = 0;
   for (i = 0; i < n; ++i)
     if (A[i] > 0 && A[i] <= n)
       B[A[i] - 1]++;
   for (i = 0; i < n; ++i)
-    if (B[i] > max)
-    {
-      max = B[i];
-      k = i;
-    }
-  delete[] B;
-  if (max > n / 2)
-    return k + 1;
-  else
-    return -1;
-}
-
+    if (B[i] > n/2)
+    {max=B[i];
+     k=i;}
+ if(k!=0)
+   return k+1;
+ else
+   reutrn -1;
 int main()
 {
   int A[8] = {0, 5, 5, 5, 5, 1, 5, 7};
